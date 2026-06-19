6540a2c9c0cd00fdf57793ef931ab8f6120ca0d4
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-06-19 10:27:25 +0530

    Update fetch URL in About2 component

--- src/Components/About2.jsx
+++ src/Components/About2.jsx
@@ -172,7 +172,7 @@ export default function AboutUsPage() {
   useEffect(() => {
     
     try{
-      fetch("https://back-bulding-code.onrender.com/story").then((res) => res.json()).then((data) => {
+      fetch("https://back-bulding-code-ofzs.onrender.com/story").then((res) => res.json()).then((data) => {
           console.log("Fetched media data:", data);
           let reverse_data = data.reverse()
            setData(reverse_data);
